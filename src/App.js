351ee2dac16003709cca959fb62131289110a585
jared <jaredrotich3@gmail.com>
2025-04-17 09:36:03 +0300

    added app. js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
+import { Outlet } from "react-router-dom";
 import NavBar from "./components/NavBar";
 
-function App() {
-  return (
-    <>
-      <header>
-        
-      </header>
-    </>
-  );
+function App(){
+    return(
+        <>
+            <header>
+                <NavBar />
+            </header>
+            <Outlet />
+        </>
+    );
 };
 
 export default App;
